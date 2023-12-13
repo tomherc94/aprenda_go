7f22033dab613feae476fc1d1dae30d63a210f61
Tomas Herculano <tomasherculano@gmail.com>
2023-12-13 16:57:12 -0300

    Exercicios nivel 1 completos

---
+++ nivel1/exercicio4/exercicio4.go
@@ -0,0 +1,26 @@
+/*- Crie um tipo. O tipo subjacente deve ser int.
+- Crie uma variável para este tipo, com o identificador "x", utilizando a palavra-chave var.
+- Na função main:
+    1. Demonstre o valor da variável "x"
+    2. Demonstre o tipo da variável "x"
+    3. Atribua 42 à variável "x" utilizando o operador "="
+    4. Demonstre o valor da variável "x"
+- Para os aventureiros: https://golang.org/ref/spec#Types
+- Agora já somos quase ninjas nível 1!
+- Solução: https://play.golang.org/p/snm4WuuYmG
+*/
+
+package main
+
+import "fmt"
+
+type meuTipo int
+
+var x meuTipo
+
+func main() {
+	fmt.Printf("%v\t%T\n", x, x)
+	x = 42
+	fmt.Printf("%v\t%T\n", x, x)
+
+}
